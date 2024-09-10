212763d050a25309bd96b7f8eb81e7c677202979
Minh Tran <minhtran.2404@outlook.com>
2024-09-10 18:10:36 +0700

    refactor: facebook crawler class

--- src/bumo_telebot/facebook_crawler.py
+++ src/bumo_telebot/facebook_crawler.py
@@ -1,6 +1,7 @@
 import logging
 import re
 import time
+from typing import Optional
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,6 +15,14 @@ class FacebookCrawler:
     PAGE_TIMEOUT = 75
 
     def __init__(self, logger: logging):
+        self.logging = logger
+
+        self._setup_Chrome()
+
+    def _setup_Chrome(self):
+        """
+        Setup Chrome options
+        """
         self.chrome_options = Options()
         self.chrome_options.add_argument("--headless")
         self.chrome_options.add_argument("--disable-gpu")
@@ -24,22 +33,14 @@ class FacebookCrawler:
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
         )
 
-        self.logging = logger
-
-    def get_latest_post(self, page_id):
+    def get_latest_post(self, page_id: str) -> Optional[str]:
+        """
+        Get the latest post from a Facebook page
+        """
         driver = webdriver.Chrome(options=self.chrome_options)
-
         page_url = f"https://www.facebook.com/{page_id}"
+        latest_post_url = None
 
-        try:
-            return self._get_latest_post(driver, page_url)
-        except Exception as ex:
-            logging.error(f"Exception: {ex}")
-            return None
-        finally:
-            driver.quit()
-
-    def _get_latest_post(self, driver, page_url):
         try:
             driver.execute_cdp_cmd(
                 "Network.setUserAgentOverride",
@@ -48,7 +49,7 @@ class FacebookCrawler:
                 },
             )
 
-            time.sleep(self.DELAY_TIME_LOAD)
+            # time.sleep(self.DELAY_TIME_LOAD)
             driver.get(page_url)
 
             wait = WebDriverWait(driver, self.PAGE_TIMEOUT)
@@ -66,33 +67,44 @@ class FacebookCrawler:
             url = link_element.get_attribute("href")
 
             logging.info(f"Found URL: {url}")
-            return self.clean_url(url)
+            latest_post_url = self.clean_url(url)
         except Exception as ex:
             logging.error(f"Exception: {ex}")
-            return None
+        finally:
+            driver.quit()
+            return latest_post_url
 
     @staticmethod
-    def find_post_link(driver):
-        links = driver.find_elements(By.TAG_NAME, "a")
-        regex = re.compile(r"https://www\.facebook\.com/[^/]+/posts/")
+    def find_post_link(driver) -> Optional[str]:
+        """
+        Find the first post link among all anchor tags
+        """
+        # Find all anchor tags
+        anchors = driver.find_elements(By.TAG_NAME, "a")
 
-        for link in links:
-            href = link.get_attribute("href")
-            if regex.match(href):
-                return link
+        # Regular expression to match Facebook post URLs
+        fb_post_regex = re.compile(r"https://www\.facebook\.com/[^/]+/posts/")
+        for anchor in anchors:
+            href = anchor.get_attribute("href")
+            if fb_post_regex.match(href):
+                return anchor
+
         return None
 
     @staticmethod
-    def clean_url(url):
-        if not url:
-            return url
-
+    def clean_url(url: str) -> str:
+        """
+        Clean the URL by removing the query string and fragment
+        """
+        # Find the indices of the query string and fragment
         query_index = url.find("?")
         fragment_index = url.find("#")
 
+        # If neither the query string nor the fragment is found, return the URL as is
         if query_index == -1 and fragment_index == -1:
             return url
 
+        # Find the cutoff index
         cutoff_index = (
             min(query_index, fragment_index)
             if query_index != -1 and fragment_index != -1
